36fd3345dd0e519b899c38ffc4228f1db92ef45e
axf <1460014874@qq.com>
2025-01-04 17:10:55 +0800

    feat(utils): 增加 ThreadPool 类用于异步执行任务

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 #include <thread>
 #include "box2d/box2d.h"
 #include "utils/Time.h"
+#include "utils/ThreadPool.h"
 using namespace std;
 int main()
 {
+     ThreadPool t(16);
+     for (int i = 1; i <= 10; ++i)
+     {
+          t.submit([i]()
+                   { std::cout << i << endl; });
+     }
      auto tim = Time::now();
      cout << tim.toString() << endl;
      this_thread::sleep_for(chrono::seconds(2));
      tim.update();
      cout << tim.toString();
+     t.close();
      return 0;
 }
