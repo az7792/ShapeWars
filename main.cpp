6dbd2b4ba698368052589c8181a85c4de3f7c3a9
axf <1460014874@qq.com>
2025-01-04 13:38:54 +0800

    feat(utils): 增加 Time 类用于时间处理

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <thread>
 #include "box2d/box2d.h"
+#include "utils/Time.h"
 using namespace std;
 int main()
 {
-     
+     auto tim = Time::now();
+     cout << tim.toString() << endl;
+     this_thread::sleep_for(chrono::seconds(2));
+     tim.update();
+     cout << tim.toString();
      return 0;
 }
