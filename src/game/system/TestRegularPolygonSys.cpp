bc0621897c3826c027b371b064ef4ccd2725db9f
axf <1460014874@qq.com>
2025-02-26 21:10:50 +0800

    feat(RegularPolygonSys): 添加更丰富的方块生成
    不同的积分，大小，血量，巨大化

--- src/game/system/TestRegularPolygonSys.cpp
+++ src/game/system/TestRegularPolygonSys.cpp
@@ -17,24 +17,26 @@ namespace
 
           // 40：40：15：5
           if (randNum < 40)
-               params.polygon = {3, 3.f / 10};
+               params.polygon = {3, 3.f / 10}, params.score = 10, params.initialHP = params.maxHP = 50;
           else if (randNum < 80)
-               params.polygon = {4, 4.f / 10};
+               params.polygon = {4, 4.f / 10}, params.score = 20, params.initialHP = params.maxHP = 100;
           else if (randNum < 95)
-               params.polygon = {5, 5.f / 10};
+               params.polygon = {5, 6.f / 10}, params.score = 40, params.initialHP = params.maxHP = 150;
           else
-               params.polygon = {6, 6.f / 10};
+               params.polygon = {6, 7.f / 10}, params.score = 50, params.initialHP = params.maxHP = 400;
 
           randNum = std::rand() % 100;
           if (randNum < 50)
-               params.style = {BOLCK_FILL_GREEN, BOLCK_STROKE_GREEN};
+               params.style = {BOLCK_FILL_GREEN, BOLCK_STROKE_GREEN}, params.score *= 1;
           else if (randNum < 80)
-               params.style = {BOLCK_FILL_BLUE, BOLCK_STROKE_BLUE};
+               params.style = {BOLCK_FILL_BLUE, BOLCK_STROKE_BLUE}, params.score *= 2;
           else if (randNum < 90)
-               params.style = {BOLCK_FILL_PURPLE, BOLCK_STROKE_PURPLE};
+               params.style = {BOLCK_FILL_PURPLE, BOLCK_STROKE_PURPLE}, params.score *= 3;
           else
-               params.style = {BOLCK_FILL_ORANGE, BOLCK_STROKE_ORANGE};
+               params.style = {BOLCK_FILL_ORANGE, BOLCK_STROKE_ORANGE}, params.score *= 4;
 
+          if (rand() % 100 == 0)
+               params.polygon.radius = 1.5f, params.score *= 30, params.initialHP = params.maxHP *= 50;
           return params;
      }
 }
